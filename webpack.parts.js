7f7d10a7be33a15685256767aa8141c5b1e3edd0
erik <erik@hark.bz>
2018-05-04 19:09:54 -0400

    padding change to mobile menu.
    fixed webpack image compression config (deprecation warnings)

--- webpack.parts.js
+++ webpack.parts.js
@@ -9,283 +9,280 @@ const publicPath = '/'
 
 exports.publicPath = publicPath
 
-exports.devServer = ({ host, port } = {}) => ({
-  devServer: {
-    watchOptions: {
-      ignored: /node_modules/
-    },
-    publicPath,
-    // Enable history API fallback so HTML5 History API based
-    // routing works. Good for complex setups.
-    historyApiFallback: true,
+exports.devServer = ({host, port} = {}) => ({
+	devServer: {
+		watchOptions: {
+			ignored: /node_modules/
+		},
+		publicPath,
+		// Enable history API fallback so HTML5 History API based
+		// routing works. Good for complex setups.
+		historyApiFallback: true,
 
-    // Display only errors to reduce the amount of output.
-    stats: 'errors-only',
+		// Display only errors to reduce the amount of output.
+		stats: 'errors-only',
 
-    // Parse host and port from env to allow customization.
-    //
-    // If you use Docker, Vagrant or Cloud9, set
-    // host: options.host || '0.0.0.0';
-    //
-    // 0.0.0.0 is available to all network devices
-    // unlike default `localhost`.
-    host, // Defaults to `localhost`
-    port, // Defaults to 8080
+		// Parse host and port from env to allow customization.
+		//
+		// If you use Docker, Vagrant or Cloud9, set
+		// host: options.host || '0.0.0.0';
+		//
+		// 0.0.0.0 is available to all network devices
+		// unlike default `localhost`.
+		host, // Defaults to `localhost`
+		port, // Defaults to 8080
 
-    // overlay: true is equivalent
-    overlay: {
-      errors: true,
-      warnings: false
-    }
-  }
+		// overlay: true is equivalent
+		overlay: {
+			errors: true,
+			warnings: false
+		}
+	}
 })
 
 exports.loadPug = (options) => ({
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        use: [
-          {
-            loader: 'html-loader'
-          },
-          {
-            loader: 'pug-html-loader',
-            options
-          }
-        ]
-      }
-    ]
-  }
+	module: {
+		rules: [
+			{
+				test: /\.pug$/,
+				use: [
+					{
+						loader: 'html-loader'
+					},
+					{
+						loader: 'pug-html-loader',
+						options
+					}
+				]
+			}
+		]
+	}
 })
 
 exports.extractBundles = (bundles) => ({
-  plugins: bundles.map((bundle) => (
-    new webpack.optimize.CommonsChunkPlugin(bundle)
-  ))
+	plugins: bundles.map((bundle) => (
+		new webpack.optimize.CommonsChunkPlugin(bundle)
+	))
 })
 
-exports.lintJS = ({ include, exclude, options }) => ({
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include,
-        exclude,
-        enforce: 'pre',
-        loader: 'eslint-loader',
-        options
-      }
-    ]
-  }
+exports.lintJS = ({include, exclude, options}) => ({
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include,
+				exclude,
+				enforce: 'pre',
+				loader: 'eslint-loader',
+				options
+			}
+		]
+	}
 })
 
 const sharedCSSLoaders = [
-  {
-    loader: 'css-loader',
-    options: {
-      localIdentName: '[hash:base64:5]'
-    }
-  }
+	{
+		loader: 'css-loader',
+		options: {
+			localIdentName: '[hash:base64:5]'
+		}
+	}
 ]
 
 exports.autoprefix = () => ({
-  loader: 'postcss-loader',
-  options: {
-    plugins: () => [require('autoprefixer')]
-  }
+	loader: 'postcss-loader',
+	options: {
+		plugins: () => [require('autoprefixer')]
+	}
 })
 
 exports.purifyCSS = (options) => ({
-  plugins: [
-    new PurifyCSSPlugin(options)
-  ]
+	plugins: [
+		new PurifyCSSPlugin(options)
+	]
 })
 
-exports.minifyCSS = ({ options }) => ({
-  plugins: [
-    new OptimizeCSSAssetsPlugin({
-      cssProcessorOptions: options,
-      canPrint: true // false for analyzer
-    })
-  ]
+exports.minifyCSS = ({options}) => ({
+	plugins: [
+		new OptimizeCSSAssetsPlugin({
+			cssProcessorOptions: options,
+			canPrint: true // false for analyzer
+		})
+	]
 })
 
-exports.loadCSS = ({ include, exclude, use } = {}) => ({
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
+exports.loadCSS = ({include, exclude, use} = {}) => ({
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
 
-        include,
-        exclude,
+				include,
+				exclude,
 
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          ...sharedCSSLoaders.concat(use)
-        ]
-      }
-    ]
-  }
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					...sharedCSSLoaders.concat(use)
+				]
+			}
+		]
+	}
 })
 
-exports.extractCSS = ({ include, exclude, use } = {}) => {
-  // Output extracted CSS to a file
-  const plugin = new ExtractTextPlugin({
-    filename: 'styles/[name].[contenthash:8].css',
-    allChunks: true
-  })
+exports.extractCSS = ({include, exclude, use} = {}) => {
+	// Output extracted CSS to a file
+	const plugin = new ExtractTextPlugin({
+		filename: 'styles/[name].[contenthash:8].css',
+		allChunks: true
+	})
 
-  return {
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
+	return {
+		module: {
+			rules: [
+				{
+					test: /\.scss$/,
 
-          include,
-          exclude,
+					include,
+					exclude,
 
-          use: plugin.extract({
-            use: sharedCSSLoaders.concat(use),
-            fallback: 'style-loader'
-          })
-        }
-      ]
-    },
-    plugins: [plugin]
-  }
+					use: plugin.extract({
+						use: sharedCSSLoaders.concat(use),
+						fallback: 'style-loader'
+					})
+				}
+			]
+		},
+		plugins: [plugin]
+	}
 }
 
-exports.loadImages = ({ include, exclude, options } = {}) => ({
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg|svg)$/,
+exports.loadImages = ({include, exclude, options} = {}) => ({
+	module: {
+		rules: [
+			{
+				test: /\.(png|jpg|svg)$/,
 
-        include,
-        exclude,
+				include,
+				exclude,
 
-        use: {
-          loader: 'url-loader',
-          options
-        }
-      }
-    ]
-  }
+				use: {
+					loader: 'url-loader',
+					options
+				}
+			}
+		]
+	}
 })
 
-exports.optimizeImages = ({ include, exclude } = {}) => ({
-  module: {
-    rules: [
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
+exports.optimizeImages = ({include, exclude} = {}) => ({
+	module: {
+		rules: [
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
 
-        include,
-        exclude,
+				include,
+				exclude,
 
-        use: {
-
-          loader: 'image-webpack-loader',
-
-          options: {
-            progressive: true,
-
-            // optimizationLevel: 7,
-
-            gifsicle: {
-              interlaced: false
-            },
-
-            /*
-            mozjpeg: {
-
-            },
-
-            svgo: {
-
-            }, */
-
-            pngquant: {
-              quality: '65-90',
-              speed: 4
-            }
-          }
-        }
-      }
-    ]
-  }
+				use: {
+					loader: 'image-webpack-loader',
+					options: {
+						bypassOnDebug: true,
+						mozjpeg: {
+							progressive: true,
+							quality: 65
+						},
+						// optipng.enabled: false will disable optipng
+						optipng: {
+							enabled: false
+						},
+						pngquant: {
+							quality: '65-90',
+							speed: 4
+						},
+						gifsicle: {
+							interlaced: false
+						},
+						// the webp option will enable WEBP
+						webp: {
+							quality: 75
+						}
+					}
+				}
+			}
+		]
+	}
 })
 
-exports.loadFonts = ({ include, exclude, options } = {}) => ({
-  module: {
-    rules: [
-      {
-        // Capture eot, ttf, woff, and woff2
-        test: /\.(eot|ttf|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+exports.loadFonts = ({include, exclude, options} = {}) => ({
+	module: {
+		rules: [
+			{
+				// Capture eot, ttf, woff, and woff2
+				test: /\.(eot|ttf|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
 
-        include,
-        exclude,
+				include,
+				exclude,
 
-        use: {
-          loader: 'file-loader',
-          options
-        }
-      }
-    ]
-  }
+				use: {
+					loader: 'file-loader',
+					options
+				}
+			}
+		]
+	}
 })
 
-exports.loadJS = ({ include, exclude, options } = {}) => ({
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
+exports.loadJS = ({include, exclude, options} = {}) => ({
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
 
-        include,
-        exclude,
+				include,
+				exclude,
 
-        loader: 'babel-loader',
-        options
-      }
-    ]
-  }
+				loader: 'babel-loader',
+				options
+			}
+		]
+	}
 })
 
 exports.minifyJS = () => ({
-  plugins: [
-    new BabiliPlugin()
-  ]
+	plugins: [
+		new BabiliPlugin()
+	]
 })
 
 exports.setFreeVariable = (key, value) => {
-  const env = {}
-  env[key] = JSON.stringify(value)
+	const env = {}
+	env[key] = JSON.stringify(value)
 
-  return {
-    plugins: [
-      new webpack.DefinePlugin(env)
-    ]
-  }
+	return {
+		plugins: [
+			new webpack.DefinePlugin(env)
+		]
+	}
 }
 
 exports.page = ({
-  path = '',
-  template = require.resolve(
-    'html-webpack-plugin/default_index.ejs'
-  ),
-  title,
-  entry,
-  chunks
-} = {}) => ({
-  entry,
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: `${path && path + '/'}index.html`,
-      template,
-      title,
-      chunks
-    })
-  ]
+	                path = '',
+	                template = require.resolve(
+		                'html-webpack-plugin/default_index.ejs'
+	                ),
+	                title,
+	                entry,
+	                chunks
+                } = {}) => ({
+	entry,
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: `${path && path + '/'}index.html`,
+			template,
+			title,
+			chunks
+		})
+	]
 })
